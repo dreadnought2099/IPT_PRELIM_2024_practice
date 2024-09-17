e565918b165de3d094022d265ae4087cf89d8936
Raymart Magallanes <raymart.magallanes@bisu.edu.ph>
2024-09-17 11:35:10 +0800

    changes

--- resources/views/item.blade.php
+++ resources/views/item.blade.php
@@ -5,10 +5,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <div class="container">
+    <div class="container mt-4">
         <h1>Item Page</h1>
         <div class="row">
-            <div class="col-md-4">
+            <div class="col-md-4 mb-4 mr-2">
                 <div class="card">
                     <img src={{asset('images/pastel.jpg') }} class="card-img-top" alt="Item 1">
                     <div class="card-body">
@@ -16,7 +16,14 @@
                     </div>
                 </div>
             </div>
-            <!-- Add two more items similarly -->
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <img src={{asset('images/9fbbb7b9e9741a797798e77006e8f6db.jpg') }} class="card-img-top" alt="Item 2">
+                    <div class="card-body">
+                        <p class="card-text">Item 2 Description</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </body>
