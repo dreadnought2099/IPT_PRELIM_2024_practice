88a0a923dcbda46af875e69ea6e2e15930a78c43
Raymart Magallanes <raymart.magallanes@bisu.edu.ph>
2024-09-19 09:53:28 +0800

    update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,20 @@
+// <!DOCTYPE html>
+// <html>
+// <head>
+//     <title>Home Page</title>
+//     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+// </head>
+// <body>
+//     <div class="container">
+//         <h1>Home Page</h1>
+//         <nav>
+//             <a href="{{ route('item.page') }}">Item Page</a> |
+//             <a href="{{ route('picture.page') }}">Picture Page</a>
+//         </nav>
+//     </div>
+// </body>
+// </html>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,9 +22,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <div class="container">
+    <div class="container mt-4">
         <h1>Home Page</h1>
         <nav>
+            <a>Home Page</a> |
             <a href="{{ route('item.page') }}">Item Page</a> |
             <a href="{{ route('picture.page') }}">Picture Page</a>
         </nav>
