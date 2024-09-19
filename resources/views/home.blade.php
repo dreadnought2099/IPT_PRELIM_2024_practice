8476812bfa8f65072cf3954767f86ae8bef95a9b
Raymart Magallanes <raymart.magallanes@bisu.edu.ph>
2024-09-19 10:29:12 +0800

    update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,3 @@
-// <!DOCTYPE html>
-// <html>
-// <head>
-//     <title>Home Page</title>
-//     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-// </head>
-// <body>
-//     <div class="container">
-//         <h1>Home Page</h1>
-//         <nav>
-//             <a href="{{ route('item.page') }}">Item Page</a> |
-//             <a href="{{ route('picture.page') }}">Picture Page</a>
-//         </nav>
-//     </div>
-// </body>
-// </html>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -29,7 +12,7 @@
             <a href="{{ route('item.page') }}">Item Page</a> |
             <a href="{{ route('picture.page') }}">Picture Page</a>
         </nav>
+        <img class="img-fluid" src="/images/3840x2160.jpg" alt="Guardians of the Galaxy">
     </div>
 </body>
 </html>
-
